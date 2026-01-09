ee155ceed7b49e32754afb720ac83ef0023fb0c1
GiovaniLJ <giovanileonelp@gmail.com>
2026-01-08 22:54:57 -0600

    Add initial implementation of data types demonstration

---
+++ Tipos_de_datos.py
@@ -0,0 +1,22 @@
+empleado = "Juan Pablo Martinez Jiron"
+edad = 18
+estatura = 1.75
+gerentedehuipix = True
+
+estatura_float_to_str_variable= str(estatura)
+print(estatura_float_to_str_variable)
+print(type(estatura_float_to_str_variable))
+
+print(f"Tipo de nombre: {type(empleado)}")
+print(f"Tipo de estatura: {type(estatura)}")
+print(f"¿Es gerente de Huipix= {gerentedehuipix}")
+ #Concatenacion de variables en un print
+print(empleado,edad,estatura,gerentedehuipix)
+
+#Tipos de datos
+print(type("Por el emperador"))# Tipo str
+print(type(5)) # Tipo Int
+print(type(1.45)) #Tipo Float
+print(type(3 + 1j)) #Tipo complex
+print(type(True)) #Tipo bool
+
