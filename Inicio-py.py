0027e4f3fed4218d1f76412f9c16acabdef224b8
GiovaniLJ <giovanileonelp@gmail.com>
2026-01-08 21:20:02 -0600

    first commit

---
+++ Inicio-py.py
@@ -0,0 +1,11 @@
+nombre = "Futuro ingeniero en Suiza"
+horas_estudio = 0
+
+
+while horas_estudio < 3:
+    print(f"sigue precticando, {nombre}. llevas {horas_estudio}.")
+    horas_estudio += 1
+print("¡Primera meta del dia cumplida!")
+
+meta = "ingeniero de IA en Suiza"
+print(f"iniciando el camino hacia: {meta}")
---
+++ hola_suiza.py
@@ -0,0 +1,13 @@
+meta = "Ingeniero de IA en Suiza"
+print(f"Iniciando el camino hacia: {meta}")
+
+inicio = 2026
+meta= 2030
+faltan = meta - inicio
+print(f"Faltan {faltan}años para ser ingeniero. !A Darle¡")
+
+nombre = "Giovani Leonel"
+dias_por_año = 365
+años_carrera = 4
+total_dias = dias_por_año * años_carrera
+print(f"Hola{nombre}, En {años_carrera} años habras estudiado aproximandamente {total_dias} dias en total, Tu puedes sigue adelante")
